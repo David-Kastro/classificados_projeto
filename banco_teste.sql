e8d560411bb4b1c12083aa526258935d1e6e2784
David <daviduartedf@gmail.com>
2018-09-06 14:19:10 -0300

    Adicionado arquivo com a estrutura do banco de dados

--- banco_teste.sql
+++ banco_teste.sql
@@ -1,61 +1,93 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           5.6.17 - MySQL Community Server (GPL)
--- OS do Servidor:               Win32
--- HeidiSQL Versão:              9.4.0.5125
--- --------------------------------------------------------
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: teste
+-- ------------------------------------------------------
+-- Server version	5.6.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Copiando dados para a tabela teste.anuncios: ~5 rows (aproximadamente)
-/*!40000 ALTER TABLE `anuncios` DISABLE KEYS */;
-INSERT INTO `anuncios` (`id`, `id_usuario`, `id_categoria`, `titulo`, `descricao`, `valor`, `estado`) VALUES
-	(23, 2, 1, 'Relogio Y', 'Principais CaracterÃ­sticas:\r\n- SuperfÃ­cie geada com prego, textura especial e design contraÃ­do \r\n- AlÃ§a de couro aberta, respirÃ¡vel e confortÃ¡vel de usar \r\n- TrÃªs sub-seqÃ¼Ãªncias decorativas aumentam o grau de moda \r\n- A fivela de pino tradicional Ã© sempre design clÃ¡ssico \r\n- Uma boa escolha para um jovem ', 2000, 1),
-	(24, 3, 3, 'Placa de video', 'qqqqqqqqqqq', 2000, 1),
-	(25, 2, 4, 'Carro X', 'lol esse carro kkkkkkk!!!', 20000, 2),
-	(26, 2, 2, 'Casaco irado!!!', 'Casaco muito maneiro', 200.5, 1),
-	(27, 2, 2, 'BonÃ© ATR', 'boneeeeeeeeeeeee', 89.99, 1),
-	(28, 2, 5, 'boneco', 'sssssssssssssssssssss', 20, 1);
-/*!40000 ALTER TABLE `anuncios` ENABLE KEYS */;
+--
+-- Table structure for table `anuncio_imagens`
+--
 
--- Copiando dados para a tabela teste.anuncio_imagens: ~9 rows (aproximadamente)
-/*!40000 ALTER TABLE `anuncio_imagens` DISABLE KEYS */;
-INSERT INTO `anuncio_imagens` (`id`, `id_anuncio`, `url`) VALUES
-	(1, 2, 'cool.jpg'),
-	(68, 23, '25efb5eb59799ee72c03c9fcf30c16ad.jpg'),
-	(76, 24, '0060070e79217d29678be54774ecec7e.jpg'),
-	(77, 24, 'c9b0decfde44495b3e9218d37f57335d.jpg'),
-	(78, 25, 'caf20b9bf87cf9fbbfb5f9e5a9a93e2e.jpg'),
-	(79, 26, '404da9fff77b252fe337161caa0d4238.jpg'),
-	(80, 27, 'c01822e6bde828a5853fe5b610da9f59.jpg'),
-	(81, 23, '71bf3eaa83eded1255fe918ae2d0d36e.jpg');
-/*!40000 ALTER TABLE `anuncio_imagens` ENABLE KEYS */;
+DROP TABLE IF EXISTS `anuncio_imagens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `anuncio_imagens` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_anuncio` int(11) unsigned NOT NULL,
+  `url` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela teste.categorias: ~8 rows (aproximadamente)
-/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-INSERT INTO `categorias` (`id`, `nome`) VALUES
-	(1, 'relógios'),
-	(2, 'roupas'),
-	(3, 'eletrônicos'),
-	(4, 'carros'),
-	(5, 'brinquedos'),
-	(6, 'serviços'),
-	(7, 'pesca'),
-	(8, 'cosméticos');
-/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+--
+-- Table structure for table `anuncios`
+--
 
--- Copiando dados para a tabela teste.usuarios: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `telefone`) VALUES
-	(2, 'David', 'daviduartedf@gmail.com', '4d470b2a7883f11ddba3152497c88ef5', '(61)99999-6666'),
-	(3, 'Beltrano', 'beltrano@gmail.com', 'c0acd58b083501f155a936217c16060f', ''),
-	(4, 'Danilo', 'danilo@gmail.com', 'e94d51a35484755a9f9672d13687f499', '(61)99999-6666');
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+DROP TABLE IF EXISTS `anuncios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `anuncios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) unsigned NOT NULL,
+  `id_categoria` int(11) unsigned NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descricao` text,
+  `valor` float unsigned NOT NULL DEFAULT '0',
+  `estado` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `telefone` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-06 14:15:41
